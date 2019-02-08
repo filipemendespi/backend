d066196ecbeee3e44abd502df19b04d268872a61
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2019-02-08 12:25:50 -0200

    Add eventstore databases to docker setup

--- priv/docker/postgres/init-emcasa.sql
+++ priv/docker/postgres/init-emcasa.sql
@@ -1,2 +1,6 @@
 CREATE DATABASE re_dev;
 CREATE DATABASE re_test;
+CREATE DATABASE re_tags_readstore_dev;
+CREATE DATABASE re_tags_readstore_test;
+CREATE DATABASE re_tags_eventstore_dev;
+CREATE DATABASE re_tags_eventstore_test;
